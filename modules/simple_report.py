874b099ca23ce433a295b5b93c098ab5fdd1c361
Michal Melewski <michal.melewski@gmail.com>
2018-03-05 00:57:55 +0100

    Adding function xrefs

--- modules/simple_report.py
+++ modules/simple_report.py
@@ -12,7 +12,7 @@ supported_arch = [
 ]
 
 class Report:
-  def __init__(self):
+  def __init__(self, bv):
     self.function_data = {
     # 'function_name': {'start':0x08040000,
     #                   'blocks':10,
@@ -21,14 +21,16 @@ class Report:
     }
 
     self.templates = {}
+    self.bv = bv
 
   def __get_function_row(self, name, data):
     template = '''
     <tr>
     <td class='f_name'>0x{start:x}: {name}</td>
-    <td>{blocks}</td>
     <td>{instructions}</td>
+    <td>{blocks}</td>
     <td>{calls}</td>
+    <td>{xrefs}</td>
     </tr>
     '''
     print data['calls']
@@ -36,32 +38,35 @@ class Report:
                            name = name,
                            blocks = data['blocks'],
                            instructions = data['instructions'],
-                           calls = data['calls']
+                           calls = data['calls'],
+                           xrefs = data['xrefs']
                            )
 
 
   def add_function(self, f):
     bn.log_info('[+] Adding function')
     b, i, c = 0, 0, 0
+    x = len(self.bv.get_code_refs(f.start))
 
     for block in f.low_level_il:
-      b = b + 1
+      b += 1
       for inst in block:
-        i = i + 1
+        i += 1
         if inst.operation == bn.LowLevelILOperation.LLIL_CALL:
-          c = c + 1
+          c += 1
 
     self.function_data[f.name] = {'start': f.start,
                                   'blocks': b,
                                   'instructions': i,
-                                  'calls': c
+                                  'calls': c,
+                                  'xrefs': x
                                   }
 
   def generate_html(self):
     html = Template(self.templates['simple_report.html'])
     table = ''
 
-    for name, data in self.function_data.iteritems():
+    for name, data in sorted(self.function_data.iteritems(), reverse=True, key=lambda x: x[1]['instructions']):
       table = table + self.__get_function_row(name, data)
 
     return html.substitute(f_number = len(self.function_data.keys()),
@@ -71,19 +76,18 @@ class Report:
     with open(bn.user_plugin_path + '/sweeper/data/' + template) as fh:
       self.templates[template] = fh.read()
 
-# Function to be executed when we invoke plugin
 def run_plugin(bv, function):
   # Supported platform check
   if bv.platform.name not in supported_arch:
     log_error('[x] Right now this plugin supports only the following platforms: ' + str(supported_arch))
     return -1
 
-  r = Report()
+  r = Report(bv)
   r.load_template('simple_report.html')
 
   bn.log_info('[*] Scanning functions...')
   for function in bv.functions:
-    # prefilter
-    r.add_function(function)
+    if function.symbol.type != bn.SymbolType.ImportedFunctionSymbol:
+      r.add_function(function)
 
   bn.interaction.show_html_report('Functions report', r.generate_html(), 'Not available')
