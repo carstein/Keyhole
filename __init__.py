52217c424e2ac069563b099bea42c19e79273a34
Michal Melewski <michal.melewski@gmail.com>
2018-03-05 23:07:29 +0100

    Renaming and code cleaning

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # author: carstein <michal.melewski@gmail.com>
-# Simple information about functions in program
+# 10k foot view on binary
 
 from binaryninja import PluginCommand
 
-from modules import simple_report
+from modules import function_report
 
 # register plugin
 PluginCommand.register_for_function(
-  "[Sweeper] Simple report",
-  "Seach binary for defined signatures",
-  simple_report.run_plugin)
+  "[Keyhole] Function report",
+  "Report about functions in binary",
+  function_report.run_plugin)
